7e80c38caba876eb94548f3426f7166c5cc0a613
Ivan.Vereschagin <Ivan.Vereschagin@casceng.com>
2024-02-21 20:52:04 +0300

    add filter

--- producer-1/src/main/java/com/example/producer1/BrokerProducerService.java
+++ producer-1/src/main/java/com/example/producer1/BrokerProducerService.java
@@ -7,16 +7,19 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BrokerProducerService {
     private final KafkaTemplate<String, StreamMessage> kafkaTemplate;
+    private final static Random RND = new Random();
 
     @Scheduled(fixedDelay = 2000)
     private void sendMessage() {
-        StreamMessage message = new StreamMessage("some topic", "Some message", true);
+        int number = RND.nextInt(10) + 1;
+        StreamMessage message = new StreamMessage("some topic", "Some message", number > 5);
         kafkaTemplate.send("process-data", UUID.randomUUID().toString(), message);
     }
 }
--- streams/src/main/java/com/example/streams/MessageProcessor.java
+++ streams/src/main/java/com/example/streams/MessageProcessor.java
@@ -2,11 +2,14 @@ package com.example.streams;
 
 import com.example.dtos.StreamMessage;
 import com.example.streams.serialization.MessageSerdes;
+import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.BranchedKStream;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +17,20 @@ import org.springframework.stereotype.Component;
 public class MessageProcessor {
     private static final Serde<String> STRING_SERDE = Serdes.String();
     private static final MessageSerdes OBJECT_SERDE = new MessageSerdes();
+
+    private static final Predicate<String, StreamMessage> POSITIVE = (key, value) -> value.isImportant();
+    private static final Predicate<String, StreamMessage> NEGATIVE = (key, value) -> !value.isImportant();
+
     @Autowired
     void processMessage(StreamsBuilder streamsBuilder) {
         KStream<String, StreamMessage> messageStream = streamsBuilder
                 .stream("process-data", Consumed.with(STRING_SERDE, OBJECT_SERDE));
         messageStream.foreach((key, value) -> System.out.println(key + ": " + value));
-        messageStream.to("receive3");
+        KStream<String, StreamMessage> importantMessages = messageStream
+                .filter(POSITIVE)
+                .mapValues(value -> new StreamMessage("Важно!!!", value.message(), false));
+        KStream<String, StreamMessage> notImportantMessages = messageStream.filter(NEGATIVE);
+        KStream<String, StreamMessage> merged = importantMessages.merge(notImportantMessages);
+        merged.to("receive3");
     }
 }
